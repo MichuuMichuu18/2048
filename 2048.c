e6f8cbfbd6c0e36bfc1ebd88f95fd3b0906cb54a
MichuuMichuu18 <111843705+MichuuMichuu18@users.noreply.github.com>
2023-05-18 13:37:12 +0200

    Code cleanup

--- 2048.c
+++ 2048.c
@@ -12,32 +12,32 @@
 #include "glut_funcs.h"
 
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    glutCreateWindow("2048");
-    
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    
-    glEnable(GL_LINE_SMOOTH);
-    glHint(GL_LINE_SMOOTH, GL_NICEST);
-
-    glEnable(GL_POINT_SMOOTH);
-    glHint(GL_POINT_SMOOTH, GL_NICEST);
-
-    init();
-    
-    glutReshapeFunc(reshape);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(keyboard);
-    glutMouseFunc(mouse);
-    
-    glutMainLoop();
-
-    return 0;
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize(500, 500);
+  glutCreateWindow("2048");
+  
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+  
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  
+  glEnable(GL_LINE_SMOOTH);
+  glHint(GL_LINE_SMOOTH, GL_NICEST);
+  
+  glEnable(GL_POINT_SMOOTH);
+  glHint(GL_POINT_SMOOTH, GL_NICEST);
+  
+  init();
+  
+  glutReshapeFunc(reshape);
+  glutDisplayFunc(display);
+  glutKeyboardFunc(keyboard);
+  glutMouseFunc(mouse);
+  
+  glutMainLoop();
+  
+  return 0;
 }
